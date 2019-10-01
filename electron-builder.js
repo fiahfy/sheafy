24b21f7f32b1041352786efb794dedeb82941fa4
fiahfy <d.fiahfy@gmail.com>
2019-10-01 22:03:23 +0900

    Fix plugins

--- electron-builder.js
+++ electron-builder.js
@@ -1,12 +1,6 @@
 module.exports = {
   appId: 'net.fiahfy.sheafy',
   files: ['app', 'main.js', 'preload.js'],
-  mac: {
-    target: ['dmg']
-  },
-  win: {
-    target: ['nsis']
-  },
   nsis: {
     oneClick: false,
     allowToChangeInstallationDirectory: true
