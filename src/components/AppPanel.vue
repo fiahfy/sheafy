a7bac3ba474cd1175756124c7a411b201759e9cc
fiahfy <d.fiahfy@gmail.com>
2019-10-20 20:30:34 +0900

    Fix for scroll position if sorted tabbar is activated

--- src/components/AppPanel.vue
+++ src/components/AppPanel.vue
@@ -57,6 +57,13 @@ export default {
         return
       }
       this.$nextTick(() => {
+        // Disabled transform for vuedraggable
+        this.$el.querySelectorAll('.app-tab-list').forEach((el) => {
+          el.parentElement.style.transform = 'none'
+          el.querySelectorAll('.app-tab-list-item').forEach((el) => {
+            el.parentElement.style.transform = 'none'
+          })
+        })
         const tab = this.$el.querySelector(
           '.app-tab-list-item.v-list-item--active'
         )
