7f93341c1498e27d31c4189d31d3aedfc806e63f
fiahfy <d.fiahfy@gmail.com>
2019-10-27 22:26:41 +0900

    Update ipc.js

--- src/plugins/ipc.js
+++ src/plugins/ipc.js
@@ -36,18 +36,6 @@ export default ({ store }) => {
     el && el.focus()
     el && el.select()
   })
-  ipcRenderer.on('goBack', () => {
-    store.$eventBus.$emit('goBack')
-  })
-  ipcRenderer.on('goForward', () => {
-    store.$eventBus.$emit('goForward')
-  })
-  ipcRenderer.on('reload', () => {
-    store.$eventBus.$emit('reload')
-  })
-  ipcRenderer.on('forceReload', () => {
-    store.$eventBus.$emit('forceReload')
-  })
   ipcRenderer.on('search', () => {
     const id = store.state.tab.activeId
     store.dispatch('tab/updateTab', { id, searching: true })
@@ -63,6 +51,18 @@ export default ({ store }) => {
   })
   ipcRenderer.on('showDownloads', () => {
     store.commit('setPanelId', { panelId: 'downloads' })
+  })
+  ipcRenderer.on('reload', () => {
+    store.$eventBus.$emit('reload')
+  })
+  ipcRenderer.on('forceReload', () => {
+    store.$eventBus.$emit('forceReload')
+  })
+  ipcRenderer.on('goBack', () => {
+    store.$eventBus.$emit('goBack')
+  })
+  ipcRenderer.on('goForward', () => {
+    store.$eventBus.$emit('goForward')
   })
   // TODO: https://github.com/electron/electron/issues/15728
   ipcRenderer.on('undo', () => {
